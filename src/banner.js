555429896f2aea46957655e52d112dc93d2a5f09
AMAN <nottherealaman@gmail.com>
2022-12-21 18:48:55 +0530

    added new style to navbar

--- src/banner.js
+++ src/banner.js
@@ -1,7 +1,7 @@
 import {useState ,useEffect} from 'react'
 import axios from './axios'
 import requests from './request'
-
+import Navbar from './navbar'
 
 function Banner() {
 
@@ -25,18 +25,23 @@ function Banner() {
 
    
   return (
-    <header className=' text-white w-screen h-screen  bg-cover bg-no-repeat	border-solid border-black border-2'  style={{backgroundImage:`url(${bg})`}}>
-      <div>
-      <h1 className=' text-start '>{ (load ? 'loading' : movies.title)}</h1>
-      <div className='flex gap-3  font-bold text-white'>
-        <button className='bg-slate-600 opacity-50'>Play</button>
-        <button className=' bg-slate-600 opacity-50'>My List</button>
+    <>
+    
+    <header className=' text-white  w-screen h-screen  bg-cover bg-no-repeat	border-solid border-black border-2 '  style={{backgroundImage:`url(${bg})`}}>
+      <Navbar />
+      
+      <div className=' p-3 pl-7 flex flex-col h-screen justify-center '>
+      <h1 className=' text-start font-bold text-5xl'>{ (load ? 'loading' : movies.title)}</h1>
+      <div className='flex gap-5  font-bold text-white p-3'>
+        <button className='bg-slate-800 opacity-75 px-3  text-center'>Play</button>
+        <button className=' bg-slate-800 opacity-75 px-3 text-center'>My List</button>
       </div>
-      <p className='text-justify w-2/3'>{ (load ? 'loading' : movies.overview)}</p>
+      <p className='text-justify w-1/2'>{ (load ? 'loading' : movies.overview)}</p>
       </div>
 
 
     </header>
+    </>
   )
 }
 
--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Avatar from './assests/avatar.svg'
+import Netflix from './assests/netflix.svg'
 
 export default function Navbar() {
   return (
-    <div>
+    <nav className='flex justify-between align-top '>
+       
+        <img src={Netflix} alt='Netflix logo' width='140' height='100' className=' scale-275 pl-6'  />
+        <img src={Avatar} alt='Avatar image' className='w-9 mr-5 ' />
       
-    </div>
+
+    </nav>
   )
 }
