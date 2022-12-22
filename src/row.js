be9a94f8f329a9028f740c08fd8937cdd0c393b6
AMAN <nottherealaman@gmail.com>
2022-12-22 09:40:15 +0530

    added styling

--- src/row.js
+++ src/row.js
@@ -1,9 +1,6 @@
 import {useState, useEffect} from 'react'
 import axios from './axios.js'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import Modal from '@mui/material/Modal'
+
 
 function Row({title , fetchUrl , islargerow}) {
 
@@ -19,51 +16,30 @@ function Row({title , fetchUrl , islargerow}) {
       })
     },[fetchUrl])
     
-    const [open, setOpen] = useState(false)
-    const handleOpen = () => setOpen(true)
-    const handleClose = () => setOpen(false)
+    
 
     
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: '#000',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-        color:'#fff'
-      }
-
+    
 
   return (
-    <>
+    
     <div className=' text-white bg-black'>
         <h2 className='font-extrabold p-2 text-start  text-xl '> {title}</h2>
          <div  className='flex gap-2 overflow-x-scroll overflow-y-hidden p-2 no-scrollbar ml-3 border-solid border-white'>
           {
             movies &&
             movies.results.map((d)=>(
-                <>
-              <img  key={d.id} src={baseurl+(islargerow ? d.poster_path : d.backdrop_path)} alt={d.name}  onClick={handleOpen} className='cursor-pointer w-1/5 hover:scale-110 hover:ease-in duration-300 p-2 rounded' />
-              
-      <Modal
-        open={open}
-        onClose={handleClose} >
-        <Box sx={style}>
-          Hi
-        </Box>
-      </Modal>
-              </>
+                
+              <img  key={d.id} src={baseurl+(islargerow ? d?.poster_path : d?.backdrop_path)} alt={d.name}   className='cursor-pointer w-1/5 hover:scale-110 hover:ease-in duration-300 p-2 rounded' />
+             
 
             ))
           }
           
          </div>
          </div>
-    </>
+         
+    
   )
 }
 
